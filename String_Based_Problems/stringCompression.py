ea0aaba40d41042e054bc5ce39cd3e082e5bdf82
Sandipan <sandipan.sk7@gmail.com>
2026-03-17 02:37:54 +0530

    String Based Upload

---
+++ String_Based_Problems/stringCompression.py
@@ -0,0 +1,15 @@
+s = input()
+
+count = 1
+res = ""
+
+for i in range(len(s)-1):
+    if s[i] == s[i+1]:
+        count += 1
+    else:
+        res += s[i] + str(count)
+        count = 1
+
+res += s[-1] + str(count)
+
+print(res)
